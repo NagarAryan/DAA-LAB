320a9920daa1b1a102d743577fc3b7d8695fce4a
NagarAryan <aryannagar2002@gmail.com>
2022-07-23 17:02:04 +0530

    Updated

--- LAB1/q2.c
+++ LAB1/q2.c
@@ -1,35 +1,29 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
-{   
-    printf("Enter the size of the array \n");
+{
+    printf("Enter the size of the array and the enter the elements of the array\n");
     int size;
-    scanf("%d",&size);
+    scanf("%d", &size);
     int ar[size];
-    int i=0;
-    int secondmax=ar[0];
-    int secondmaxp=0;
-    for(i=0;i<size;i++)
+    int i = 0;
+    for (i = 0; i < size; i++)
     {
-        scanf("%d",&ar[i]);
+        scanf("%d", &ar[i]);
     }
-    int max=ar[0];
-    int maxp=0;
-    for(i=0;i<size;i++)
+    int max = ar[0];
+    int secondmax = ar[0];
+    for (i = 0; i < size; i++)
     {
-        if(ar[i]>max)
+        if (ar[i] > max)
         {
-            secondmax=max;
-            max=ar[i];
-            maxp=i;
+            max = ar[i];
         }
-        else(ar[i]>secondmax && ar[i]!=max)
+        if (ar[i] = !max && ar[i] > secondmax)
         {
-            secondmax=ar[i];
-            secondmaxp=i;
+            secondmax = ar[i];
         }
     }
-    int maxsum=0;
-    maxsum=max+secondmax;
-    printf("The max sum is :- %d",maxsum);
+    int maxsum = 0;
+    maxsum = max + secondmax;
+    printf("The max sum is :- %d", maxsum);
 }
-    
